2363947eab823787a19e904f1e139e746a0678c6
itrezzza <91061046+itrezzza@users.noreply.github.com>
2026-07-14 15:26:09 +0330

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -72,6 +72,7 @@ CREATE TABLE IF NOT EXISTS `sp_files` (
   `price` int NOT NULL,
   `status` int NOT NULL,
   `demo` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `views` bigint NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
